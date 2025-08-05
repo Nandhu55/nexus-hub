2404cd186281581aecf569b86819ea17682bce22
G NANDHISHWAR REDDY <gnreddy255@gmail.com>
2025-08-05 05:10:58 +0000

    import { clsx, type ClassValue } from "clsx" import { twMerge } from "ta

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -6,14 +7,23 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function transformGoogleDriveLink(url: string, forDownload = false): string {
-    const regex = /https:\/\/drive\.google\.com\/file\/d\/([a-zA-Z0-9_-]+)\/view\?usp=sharing/;
-    const match = url.match(regex);
-    if (match && match[1]) {
-        const fileId = match[1];
-        if (forDownload) {
-             return `https://drive.google.com/uc?export=download&id=${fileId}`;
-        }
-        return `https://drive.google.com/file/d/${fileId}/preview`;
+  if (!url || typeof url !== 'string') {
+    return '#';
+  }
+  
+  const googleDriveRegex = /drive\.google\.com\/file\/d\/([a-zA-Z0-9_-]+)/;
+  const match = url.match(googleDriveRegex);
+  
+  if (match && match[1]) {
+    const fileId = match[1];
+    if (forDownload) {
+      // This link forces a download prompt.
+      return `https://drive.google.com/uc?export=download&id=${fileId}`;
     }
-    return url;
+    // This link is for embedding and is the most reliable for viewing.
+    return `https://drive.google.com/file/d/${fileId}/preview`;
+  }
+  
+  // Return the original URL if it's not a standard Google Drive file link
+  return url;
 }
